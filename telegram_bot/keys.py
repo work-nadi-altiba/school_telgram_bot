c3c7a9d573f3d210d1b756e27a50f619419f5387
Anas-jaf <ansas4565@gmail.com>
2023-05-18 22:17:38 +0300

    صلحت مشكلة في دالة انشاء سجل العلامات المحلية

--- telegram_bot/keys.py
+++ telegram_bot/keys.py
@@ -1,2 +1,2 @@
-token = '6287103093:AAEYzAvqKoE1PnUa9eLF_lSnjhbwdk98dck'
+production_bot = '6287103093:AAEYzAvqKoE1PnUa9eLF_lSnjhbwdk98dck'
 test_bot= '6188520333:AAGHgFat35V6V2MsVQXTPufFOM1RB5cev-U'
