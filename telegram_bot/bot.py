4ea7107b65e616525090d81747336a69252631d9
Anas-jaf <ansas4565@gmail.com>
2023-11-29 20:56:53 +0300

    اضفت خيار خفي لادخال العلامات العشوائي مثلا : /All_asses 9 11

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -441,25 +441,34 @@ def init_fill(update, context):
 
 def fill_assess_arbitrary(update, context):
     user = update.message.from_user
+    range1,range2 = ['']*2
     if update.message.text == '/cancel':
         return cancel(update, context)
     else:
-        code = update.message.text.replace('/','')
+        response = update.message.text.replace('/','').split(' ')
+        if len(response) ==3 : 
+            code ,range1 , range2 = response
+        else:
+            code = response[0]
         update.message.reply_text("انتظر لحظة لو سمحت")         
         editable_assessments = context.user_data['assessments'] 
         data_to_enter_marks = context.user_data['data_to_enter_marks']  
         username , password = context.user_data['creds'][0] , context.user_data['creds'][1]
         if code == 'All_asses':
             assess_data = [i for i in editable_assessments]
+            if not len(range1):
+                range1 , range2 = int(assessment['max_mark']*.75),assessment['max_mark']
             for assessment in assess_data:
                 wanted_grades = [i for i in data_to_enter_marks if i.get('assessment_id') == assessment['gradeId']]
-                enter_marks_arbitrary_controlled_version(username,password,wanted_grades,assessment['AssesId'],int(assessment['max_mark']*.75),assessment['max_mark'])
+                enter_marks_arbitrary_controlled_version(username,password,wanted_grades,assessment['AssesId'],range1,range2)
         else:
             assess_data = [i for i in editable_assessments if i.get('code') == code][0]
             wanted_grades = [i for i in data_to_enter_marks if i.get('assessment_id') == assess_data['gradeId']]
             assess_data = [i for i in editable_assessments if i.get('code') == code][0]
             wanted_grades = [i for i in data_to_enter_marks if i.get('assessment_id') == assess_data['gradeId']]
-            enter_marks_arbitrary_controlled_version(username,password,wanted_grades,assess_data['AssesId'],assess_data['pass_mark'],assess_data['max_mark'])
+            if not len(range1):
+                range1 , range2 = assess_data['pass_mark'],assess_data['max_mark']
+            enter_marks_arbitrary_controlled_version(username,password,wanted_grades,assess_data['AssesId'] ,range1 , range2)
             update.message.reply_text("هل تريد تعبئة علامات صف اخر؟ نعم | لا",reply_markup=ReplyKeyboardMarkup([['نعم', 'لا']], one_time_keyboard=True))
             return WAITING_FOR_RESPONSE            
         # End of conversation
