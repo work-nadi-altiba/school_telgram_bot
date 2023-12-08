c286f2e0a747f4e663ef6dac3b4ec6acbdf838ae
Anas-jaf <ansas4565@gmail.com>
2023-12-08 23:52:36 +0300

    ...

--- telegram_bot/utils1.py
+++ telegram_bot/utils1.py
@@ -1520,7 +1520,7 @@ def five_names_every_class(auth, emp_username ,session=None ):
     
     return {'teacher': userInfo['name'] ,'upload_percentage' :upload_percentage , 'row_data':row_data}
 
-def convert_official_marks_doc(ods_name='send', outdir='./send_folder' ,ods_num=1,file_path=None, file_content=None):
+def convert_official_marks_doc(ods_name='send', outdir='./send_folder' ,ods_num=1,file_path=None, file_content=None , color="#ffffff"):
     ods_file = f'{ods_name}{ods_num}.ods'
     
     if file_content is None:
@@ -1535,15 +1535,15 @@ def convert_official_marks_doc(ods_name='send', outdir='./send_folder' ,ods_num=
             shutil.copy(temp_file.name, outdir+'/final_'+ods_file)
             
     os.system(f'soffice --headless --convert-to pdf:writer_pdf_Export --outdir {outdir}  {outdir}/final_{ods_file} ')
-    add_margins(f"{outdir}/final_{ods_name}{ods_num}.pdf", f"{outdir}/output_file.pdf",top_rec=30, bottom_rec=50, left_rec=68, right_rec=120)
-    add_margins(f"{outdir}/output_file.pdf", f"{outdir}/سجل العلامات الرسمي.pdf",page=1 , top_rec=60, bottom_rec=80, left_rec=70, right_rec=120)
+    add_margins(f"{outdir}/final_{ods_name}{ods_num}.pdf", f"{outdir}/output_file.pdf",top_rec=30, bottom_rec=50, left_rec=68, right_rec=120,color_name=color)
+    add_margins(f"{outdir}/output_file.pdf", f"{outdir}/سجل العلامات الرسمي.pdf",page=1 , top_rec=60, bottom_rec=80, left_rec=70, right_rec=120,color_name=color)
     split_A3_pages(f"{outdir}/output_file.pdf" , outdir)
     reorder_official_marks_to_A4(f"{outdir}/output.pdf" , f"{outdir}/reordered.pdf")
 
-    add_margins(f"{outdir}/reordered.pdf", f"{outdir}/output_file.pdf",top_rec=60, bottom_rec=50, left_rec=68, right_rec=20)
-    add_margins(f"{outdir}/output_file.pdf", f"{outdir}/output_file1.pdf",page=1 , top_rec=100, bottom_rec=80, left_rec=90, right_rec=120)
-    add_margins(f"{outdir}/output_file1.pdf", f"{outdir}/output_file2.pdf",page=50 , top_rec=100, bottom_rec=80, left_rec=70, right_rec=60)    
-    add_margins(f"{outdir}/output_file2.pdf", f"{outdir}/سجل العلامات الرسمي_A4.pdf",page=51 , top_rec=100, bottom_rec=80, left_rec=90, right_rec=120)  
+    add_margins(f"{outdir}/reordered.pdf", f"{outdir}/output_file.pdf",top_rec=60, bottom_rec=50, left_rec=68, right_rec=20,color_name=color)
+    add_margins(f"{outdir}/output_file.pdf", f"{outdir}/output_file1.pdf",page=1 , top_rec=100, bottom_rec=80, left_rec=90, right_rec=120,color_name=color)
+    add_margins(f"{outdir}/output_file1.pdf", f"{outdir}/output_file2.pdf",page=50 , top_rec=100, bottom_rec=80, left_rec=70, right_rec=60,color_name=color)
+    add_margins(f"{outdir}/output_file2.pdf", f"{outdir}/سجل العلامات الرسمي_A4.pdf",page=51 , top_rec=100, bottom_rec=80, left_rec=90, right_rec=120,color_name=color)
     delete_files_except([f"سجل العلامات الرسمي.pdf",f"سجل العلامات الرسمي_A4.pdf"], outdir)
     
 def check_file_if_official_marks_file(file_path=None, file_content=None):
